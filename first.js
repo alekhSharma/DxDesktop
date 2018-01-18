496184e2ab575c67804118f0d3803cbe5116fe2f
alekhSharma <alekh.s0302@gmail.com>
2018-01-18 15:53:41 +0530

    Update first.js

--- first.js
+++ first.js
@@ -6,6 +6,10 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
 app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res,next){
   res.sendfile(__dirname+'/public/index.html');
@@ -68,6 +72,8 @@ io.on('connection', function(client) {
       })
 });
 
+express()
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
