4fcf8bc757984ca7492660b11124f0348c669be8
alekhSharma <alekh.s0302@gmail.com>
2018-01-18 15:56:12 +0530

    Update first.js

--- first.js
+++ first.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000
 
 
 app.use(express.static(__dirname+'/public'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.get('/',function(req,res,next){
   res.sendfile(__dirname+'/public/index.html');
 })
@@ -71,10 +74,6 @@ io.on('connection', function(client) {
           });
       })
 });
-
-express()
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
 
 
