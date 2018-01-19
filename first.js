7276f857afb96e133de0997a82807c015653bf9f
Sharma <661784@cognizant.com>
2018-01-19 12:59:26 +0530

    New Architecure

---
+++ first.js
@@ -0,0 +1,78 @@
+var sfdx = require('sfdx-node');
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+
+const PORT = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.static(__dirname+'/public'));
+app.get('/',function(req,res,next){
+  res.sendfile(__dirname+'/public/index.html');
+})
+
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.get('/', (req, res) => res.render('index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// list of scratch orgs
+var list_of_orgs = sfdx.org.list();
+list_of_orgs
+  .then(function list_of_sOrg(data){
+    return data; 
+  }); 
+
+
+io.on('connection', function(client) {  
+
+   console.log('Client connected...');
+
+      
+      client.on('clicked', function() {
+        var list_of_orgs = sfdx.org.list();
+        list_of_orgs
+          .then(function(data){       
+                  //send a message to ALL connected clients
+                  console.log('inside list');
+                  io.emit('buttonUpdate', data);
+              });
+          });
+
+       client.on('ModelList', function() {
+        var list_of_orgs = sfdx.org.list();
+        list_of_orgs
+          .then(function(data){       
+                  //send a message to ALL connected clients
+                  console.log('inside list');
+                  io.emit('ListData', data);
+                  console.log(data);
+              });
+          });
+
+      client.on('createOrg',function(){
+          sfdx.org.open({
+            targetusername : 'alekh.newdx@cognizant.com'
+          }).then(function(){
+            console.log('org open');
+            io.emit('Org created','org created');
+          });
+      });
+
+    
+      client.on('OpenOrg', function()
+      {
+          //authorize a dev hub
+          sfdx.auth.webLogin({
+              setdefaultdevhubusername: true,
+              setalias: 'HubOrg'
+          })
+          .then(function(){
+            console.log('Source pushed to scratch org');  
+          });
+      })
+});
